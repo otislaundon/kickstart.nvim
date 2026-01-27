f71b6c7e23c3d7a879d821ec3dec72b4fa562fcd
otislaundon <otislaundon@gmail.com>
2026-01-27 12:52:01 +0000

    added document template to tex snips

--- LuaSnip/tex/misc.lua
+++ LuaSnip/tex/misc.lua
@@ -1,4 +1,21 @@
 return {
+  s(
+    { trig = 'template', dscr = 'Basic document template' },
+    fmt(
+      [[
+\documentclass{article}
+\usepackage{amsmath}
+\usepackage{amssymb}
+\usepackage{amsthm}
+
+\begin{document}
+	<>
+\end{document}
+  ]],
+      { i(1) },
+      { delimiters = '<>' }
+    )
+  ),
   s({ trig = 'lab', dscr = 'inserts LaTeX label command' }, fmt([[\label{<>}]], { i(1) }, { delimiters = '<>' })),
   s({ trig = 'ref', dscr = 'inserts LaTeX ref command' }, fmt([[\ref{<>}]], { i(1) }, { delimiters = '<>' })),
   s({ trig = 'fr', dscr = 'inserts LaTeX frac command' }, fmt([[\frac{<>}{<>}]], { i(1), i(2) }, { delimiters = '<>' })),
